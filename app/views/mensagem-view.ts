d75460a838a258f668612249d2795de11d94fd2d
iago-monteirog <iagomgarcia9000@gmail.com>
2022-09-13 14:13:33 -0300

    utiliza herança de classes

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,10 +1,7 @@
-export class MensagemView {
-    private elemento: HTMLElement;
-    
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
+import { View } from "./view.js";
 
+export class MensagemView extends View{
+    
     template(model: string): string {
         return `
             <p class="alert alert-info">${model}</p>
